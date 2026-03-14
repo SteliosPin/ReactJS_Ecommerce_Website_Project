474bb86981dfe2e19a23ad79b638849955dbefdb
SteliosPin <Steliospinakotas60@gmail.com>
2026-03-14 11:08:52 +0200

    Added home page

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import Home  from './pages/Home'
+import Auth from './pages/Auth'
+import Checkout from './pages/Checkout'
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import NavBar from './components/NavBar'
+
+function App() {
+
+  return (
+  <div className='app'>
+    <NavBar/>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/auth' element={<Auth/>}/>
+      <Route path='/checkout' element={<Checkout/>}/>
+    </Routes>
+    </div>  
+  )
+}
+
+export default App
